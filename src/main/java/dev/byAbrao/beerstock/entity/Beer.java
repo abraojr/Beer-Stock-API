75263718a217a76e7d544dd08411868c26242f18
Abrão Astério Junior <abcorinthians@gmail.com>
2021-07-15 22:10:26 -0300

    Developed database model and created repository class

---
+++ src/main/java/dev/byAbrao/beerstock/entity/Beer.java
@@ -0,0 +1,32 @@
+package dev.byAbrao.beerstock.entity;
+
+import dev.byAbrao.beerstock.enums.BeerType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private int max;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BeerType type;
+}
---
+++ src/main/java/dev/byAbrao/beerstock/repository/BeerRepository.java
@@ -0,0 +1,11 @@
+package dev.byAbrao.beerstock.repository;
+
+import dev.byAbrao.beerstock.entity.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByName(String name);
+}
