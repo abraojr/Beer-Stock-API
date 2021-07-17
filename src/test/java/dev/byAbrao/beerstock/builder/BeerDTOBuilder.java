f4e939786364483055f38a6578c8371f510462a8
Abrão Astério Junior <abcorinthians@gmail.com>
2021-07-17 03:33:13 -0300

    Classes that will help with testing

---
+++ src/test/java/dev/byAbrao/beerstock/builder/BeerDTOBuilder.java
@@ -0,0 +1,36 @@
+package dev.byAbrao.beerstock.builder;
+
+import dev.byAbrao.beerstock.dto.BeerDTO;
+import dev.byAbrao.beerstock.enums.BeerType;
+import lombok.Builder;
+
+@Builder
+public class BeerDTOBuilder {
+
+    @Builder.Default
+    private Long id = 1L;
+
+    @Builder.Default
+    private String name = "Brahma";
+
+    @Builder.Default
+    private String brand = "Ambev";
+
+    @Builder.Default
+    private int max = 50;
+
+    @Builder.Default
+    private int quantity = 10;
+
+    @Builder.Default
+    private BeerType type = BeerType.LARGER;
+
+    public BeerDTO toBeerDTO() {
+        return new BeerDTO(id,
+                name,
+                brand,
+                max,
+                quantity,
+                type);
+    }
+}
